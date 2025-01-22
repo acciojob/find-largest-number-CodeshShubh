7582c19a4387ecc3abe04836cee243a7e7cacbcb
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-22 19:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function findLargest(a, b, c) {
   //your code here
+	let arr = [a, b, c];
+	arr.sort((a,b)=> b - a);   
+	return arr[0];
+	
 }
 
 const num1 = parseInt(prompt("Enter First Number."));
