3b28531e06dbebc5c70560d0dae7996925c1f05b
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-22 19:46:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function findLargest(a, b, c) {
   //your code here
-	let arr = [a, b, c];
-	arr.sort((a,b)=> b - a);   
-	return arr[0];
+	// let arr = [a, b, c];
+	// for(i=arr.length-1; i>=0 ; i--){
+		
+	// }
+
+	let largestNumber = a;
+
+	if(b>largestNumber){
+		largestNumber = b
+	}else if(c>largestNumber){
+		largestNumber = c
+	}
+	return largestNumber;   
 	
 }
 
